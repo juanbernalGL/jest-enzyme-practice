ae63459e4974d117c25a600635e4c5d6b215f074
Juan Bernal <juan.bernal@Juan-Bernal-MacBook-Pro.local>
2020-11-13 11:07:38 -0500

    Configure Jest, enzyme andadapter, first test done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+/**
+ * Test React App Component with jest and Enzyme, using udemy like guide
+ * 
+ * @version 1.0
+ * @author [Juan Jose Bernal Villamarin] (https://github.com/juanbernalGL)
+ */
+import React from 'react';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a shallowWrapper for the App Component
+ * @function setup
+ * @returns { ShallowWrapper }
+ */
+const setup = () => shallow(<App/>);
+
+const findByTestAttr = (wrapper, val) => (wrapper.find(`[data-test='${val}']`));
+test('renders without errors', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+} );
